923a3a7b7d53b12576b91d53a206cb6dd8a9f0d5
roman krasilnikov <krasilnikovroman5@gmail.com>
2025-12-16 23:25:11 +0700

    React router added, styles update, product card ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes, useLocation } from 'react-router'
+import MainPage from './pages/Main/MainPage'
+import ProductPage from './pages/Product/ProductPage'
+import ProductModal from './pages/Product/ProductModal'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const location = useLocation()
+    const { state } = location
 
-  return (
-    <>
-      <div className='flex flex-row justify-center'>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <Routes location={state?.background || location}>
+                <Route path="/">
+                    <Route index element={<MainPage />} />
+                    <Route path="/products/:id" element={<ProductPage />} />
+                </Route>
+            </Routes>
+
+            {state?.background && (
+                <Routes>
+                    <Route path="/products/:id" element={<ProductModal />} />
+                </Routes>
+            )}
+        </>
+    )
 }
 
 export default App
+
---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,40 @@
+import { useLocation, useNavigate } from 'react-router'
+import type { ProductType } from '../types/ProductType'
+
+export default function ProductCard({
+    id,
+    title,
+    price,
+    image,
+    category,
+}: ProductType) {
+    const navigate = useNavigate()
+    const location = useLocation()
+
+    function handlePoductClick() {
+        navigate(`/products/${id}`, {
+            state: { background: location },
+        })
+    }
+
+    return (
+        <div className="flex w-60 flex-col items-center gap-y-4 rounded-xl border border-solid border-gray-200 bg-white p-4">
+            <img
+                src={`/product-images/${image}`}
+                className="w-full object-cover"
+            />
+
+            <div className="flex w-full flex-col items-start">
+                <p className="text-sm font-normal text-gray-500">{category}</p>
+                <p className="text-md line-clamp-1 text-start font-medium text-black">
+                    {title}
+                </p>
+                <p className="text-sm font-bold text-black">{price}₽</p>
+            </div>
+
+            <button className="w-full" onClick={handlePoductClick}>
+                Add to cart
+            </button>
+        </div>
+    )
+}
